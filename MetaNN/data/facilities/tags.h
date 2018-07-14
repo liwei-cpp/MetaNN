054ea158870eed60ad7d86d393f0901e5d8e8a91
Li Wei <liweifriends@gmail.com>
2018-07-14 16:34:19 +0800

    remove batchseq for its ambiguity

--- MetaNN/data/facilities/tags.h
+++ MetaNN/data/facilities/tags.h
@@ -6,13 +6,13 @@ namespace MetaNN
 namespace CategoryTags
 {
     struct Invalid;
+    
     struct Scalar;
     struct Matrix;
     struct ThreeDArray;
 
     template <typename> struct Batch;
     template <typename> struct Seq;
-    template <typename> struct BatchSeq;
 
     using BatchScalar = Batch<Scalar>;
     using BatchMatrix = Batch<Matrix>;
@@ -21,10 +21,6 @@ namespace CategoryTags
     using SeqScalar = Seq<Scalar>;
     using SeqMatrix = Seq<Matrix>;
     using SeqThreeDArray = Seq<ThreeDArray>;
-
-    using BatchSeqScalar = BatchSeq<Scalar>;
-    using BatchSeqMatrix = BatchSeq<Matrix>;
-    using BatchSeqThreeDArray = BatchSeq<ThreeDArray>;
 }
 
 /// device types
