1d8306b513302ac124bf3dc7bd480543c8adb232
liwei <liweifriends@gmail.com>
2020-01-26 16:50:35 +0000

    add test cases for sigmoid and tanh (Tensor edition)

--- MetaNN/meta_nn2.h
+++ MetaNN/meta_nn2.h
@@ -3,3 +3,5 @@
 #include <MetaNN/data/_.h>
 
 #include <MetaNN/operators/activations/relu.h>
+#include <MetaNN/operators/activations/sigmoid.h>
+#include <MetaNN/operators/activations/tanh.h>
--- Tests/DataOpTest2/operators/activation/_.h
+++ Tests/DataOpTest2/operators/activation/_.h
@@ -3,9 +3,13 @@
 namespace Test::Operators::Activation
 {
     void test_relu();       void test_relu_grad();
+    void test_sigmoid();    void test_sigmoid_grad();
+    void test_tanh();       void test_tanh_grad();
 
     void test()
     {
         test_relu();        test_relu_grad();
+        test_sigmoid();     test_sigmoid_grad();
+        test_tanh();        test_tanh_grad();
     }
 }
